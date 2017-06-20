15ad0d38dbe751596588ea300e8544586b58cc50
Moo778 <a.t.maruatni.cole@gmail.com>
2017-06-19 15:01:18 -1000

    good to go

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 function fortuneTeller(){
 console.log("Hello");
+var numOfChildren = 1;
+var partnersName = "Someone"
+var geographicalLocation = "Rich area of Oahu"
+var jotitle = "Professional Basketball Player"
 
+console.log( "You will be a X in Y, and married to Z with N kids.");
+console.log("You will be a" + jobtitle + "in" + geographicalLocation + ", and married to " + partnersName
 }
-
+fortuneTeller()
 function ageCalculator(){
  
 }
